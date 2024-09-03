1a23c9002637459a5606e7678f38c336db087bdb
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-02 23:17:23 -0300

    Refactor getData function to accept request parameter

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,18 +1,34 @@
 import ServiciosPage from "./components/ServiciosPage";
 import { Servicio } from "@/interfaces/servicio.interface";
 
-const getData = async (): Promise<{ servicios: Servicio[]; items: number }> => {
+const getData = async (
+  request: any
+): Promise<{ servicios: Servicio[]; items: number }> => {
+  const { month } = request.searchParams;
+  const { year } = request.searchParams;
+
+  if (month && year) {
+    const response = await fetch(
+      `http://localhost:8080/api/servicios/all?month=${month}&year=${year}`,
+      {
+        cache: "no-store",
+      }
+    );
+    const data = await response.json();
+    return data;
+  }
+
   const response = await fetch("http://localhost:8080/api/servicios/all");
   const data = await response.json();
+
   return data;
 };
 
-export default async function Page() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const data = await getData();
+export default async function Page(request: any) {
+  const data = await getData(request);
 
   return (
-    <div className="flex flex-col gap-8 w-full h-full">
+    <div className="flex flex-col gap-3 w-full h-full">
       <ServiciosPage data={data} />
     </div>
   );
