0a781a8fb07d7d9c74d33e0e0934582407dec89c
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-12 11:31:19 -0300

    Refactor password input in login form component

--- app/login/components/LoginForm.tsx
+++ app/login/components/LoginForm.tsx
@@ -4,9 +4,12 @@ import SubmitForm from "./SubmitForm";
 import { login } from "../actions";
 import { useState } from "react";
 import Link from "next/link";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
 
 const LoginForm = () => {
   const [error, setError] = useState<Record<string, any>>({});
+  const [isPassword, setIsPassword] = useState(true);
 
   return (
     <form
@@ -41,15 +44,28 @@ const LoginForm = () => {
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            required
-            name="password"
-            className={`p-2 border border-gray-300 rounded-lg ${
-              error.password && "border border-red-300"
-            }`}
-          />
+          <div className="w-full relative">
+            <input
+              type={isPassword ? "password" : "text"}
+              placeholder="Contraseña"
+              required
+              name="password"
+              className={`p-2 border border-gray-300 rounded-lg w-full ${
+                error.password && "border border-red-300"
+              }`}
+            />
+            {isPassword ? (
+              <FaEyeSlash
+                onClick={() => setIsPassword(!isPassword)}
+                className="absolute right-2 top-3 cursor-pointer"
+              />
+            ) : (
+              <FaEye
+                onClick={() => setIsPassword(!isPassword)}
+                className="absolute right-2 top-3 cursor-pointer"
+              />
+            )}
+          </div>
           {error.password && (
             <p className="text-red-500 text-sm">
               {" "}
